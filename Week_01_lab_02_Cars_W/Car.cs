387f36f9c8caccded434066f039ec0cba7c14f4d
Janiel Mark Javier <jjavie27@my.centennialcollege.ca>
2024-04-20 16:53:52 -0400

    Add project files.

---
+++ Week_01_lab_02_Cars_W/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_01_lab_02_Cars_W
+{
+    //declaring fields
+    class Car
+    {
+        private int year;
+        private string model;
+        private bool isDrivable;
+        private double price;
+
+
+        //Constructor
+        public Car(string model, int year, double price, bool isDrivable)
+        {
+            this.Year = year;
+            this.Model = model;
+            this.IsDrivable = isDrivable;
+            this.Price = price;
+        }
+
+        //getter and setter
+        public int Year { get => year; set => year = value; }
+        public string Model { get => model; set => model = value; }
+
+        public bool IsDrivable { get => isDrivable; set => isDrivable = value; }
+        public double Price { get => price; set => price = value; }
+
+        //display output as string with the below format.
+        public override string ToString()
+        {
+            return $" Model: {model}, Year: {year}, Price: {price}, IsDrivable: {IsDrivable}";
+        }
+    }
+}
+        
---
+++ Week_01_lab_02_Cars_W/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_01_lab_02_Cars_W
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //cars with 4 arguments
+            Car c = new Car("Honda", 2009, 4500.19, true);
+            Car d = new Car("Chevrolet", 2010, 6500.25, true);
+            Car e = new Car("Ford", 2011, 5500.22, true);
+            Car f = new Car("Ferrari", 2012, 9500, false);
+
+            //displaying cars with below format
+            Console.WriteLine(c.ToString() + '\n' + d.ToString() + '\n' + e.ToString() + '\n' + f.ToString());
+            Console.ReadKey();
+        }
+    }
+}
